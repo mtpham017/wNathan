1e7d46ed9c5b54b0b4dd0727c075b8df63dac26d
Nathan Pham-Le <96894478+mtpham017@users.noreply.github.com>
2024-08-28 16:28:13 -0500

    Added background videos to album boxes in home page

--- src/components/AlbumGrid.jsx
+++ src/components/AlbumGrid.jsx
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './AlbumGrid.css';
 
-const AlbumBox = ({ name, details, date }) => (
-  <div className="album-box">
-    <h1 className="album-name">{name}</h1>
-    <div className="album-info">
-      <p className="album-details">{details}</p>
-      <p className="album-date">{date}</p>
+const AlbumBox = ({ name, details, date, videoSrc }) => {
+  const videoRef = useRef(null);
+
+  const handleMouseEnter = () => {
+    if (videoRef.current) {
+      videoRef.current.play();
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (videoRef.current) {
+      videoRef.current.pause();
+      videoRef.current.currentTime = 0; // Reset video to the beginning
+    }
+  };
+
+  return (
+    <div 
+      className="album-box" 
+      onMouseEnter={handleMouseEnter} 
+      onMouseLeave={handleMouseLeave}
+    >
+      <h1 className="album-name">{name}</h1>
+      <div className="album-info">
+        <p className="album-details">{details}</p>
+        <p className="album-date">{date}</p>
+      </div>
+      <video ref={videoRef} className="background-video" muted>
+        <source src={videoSrc} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
-  </div>
-);
+  );
+};
 
 function AlbumGrid() {
   return (
     <div className="grid-container">
-      <AlbumBox name="2014 Forest Hill Drive" details="J.Cole, Hip-Hop/Rap" date="2014" />
-      <AlbumBox name="Blonde" details="Frank Ocean, Pop" date="2016" />
+      <AlbumBox 
+        name="2014 Forest Hill Drive" 
+        details="J.Cole, Hip-Hop/Rap" 
+        date="2014" 
+        videoSrc="src\assets\jcole_web.mp4"
+      />
+      <AlbumBox 
+        name="Blonde" 
+        details="Frank Ocean, Pop" 
+        date="2016" 
+        videoSrc="src\assets\frank_web.mp4"
+      />
       <div className='album-pattern'></div>
     </div>
   );
